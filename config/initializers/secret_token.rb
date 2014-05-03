9b5c5cb2952e5e9264f2bc60b649ba5d64b9ee0a
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-05-03 14:28:18 -0700

    Initial commit for project 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MortgageApp::Application.config.secret_key_base = '91b20c095c044a8ee62ca8c08a089591c852a57ceff6b572298be8216d6a73d9aa23aa0f9b13b0a3a375a59ff87e4867510c37475346f27100838efde2ea83fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MortgageApp::Application.config.session_store :cookie_store, key: '_Mortgage_App_session'
