096502b58ba0dff0221a85d268167e6fafa74933
Dante Carlesimo <dantecarlesimo@gmail.com>
2014-07-12 11:34:45 -0700

    added approval bar to scenario show page with javascript

---
+++ app/assets/javascripts/scenarios.js
@@ -0,0 +1,52 @@
+"use strict";
+
+$(function(){
+
+var value = (function(){
+  var approval_prob;
+  if ($('#30_approval').data("approval") === "Excellent"){
+    approval_prob = 50;
+  } else if ($('#30_approval').data("approval") === "Good"){
+    approval_prob = 40;
+  } else if ($('#30_approval').data("approval") === "Fair"){
+    approval_prob = 30;
+  } else {
+    approval_prob = 20;
+  }
+  return approval_prob;
+})();
+
+var value2 = (function(){
+  var approval_prob;
+  if ($('#30_approval_2').data("approval") === "Excellent"){
+    approval_prob = 50;
+  } else if ($('#30_approval_2').data("approval") === "Good"){
+    approval_prob = 40;
+  } else if ($('#30_approval_2').data("approval") === "Fair"){
+    approval_prob = 30;
+  } else {
+    approval_prob = 20;
+  }
+  return approval_prob;
+})();
+
+var value3 = (function(){
+  var approval_prob;
+  if ($('#30_approval_3').data("approval") === "Excellent"){
+    approval_prob = 50;
+  } else if ($('#30_approval_3').data("approval") === "Good"){
+    approval_prob = 40;
+  } else if ($('#30_approval_3').data("approval") === "Fair"){
+    approval_prob = 30;
+  } else {
+    approval_prob = 20;
+  }
+  return approval_prob;
+})();
+
+
+$('#30_approval').append("<meter value="+value+" min='0' max='50'></meter>" );
+$('#30_approval_2').append("<meter value="+value2+" min='0' max='50'></meter>" );
+$('#30_approval_3').append("<meter value="+value3+" min='0' max='50'></meter>" );
+
+});
